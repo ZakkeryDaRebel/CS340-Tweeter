63179d91ea6424b82a20c16e2c227a944a85bdc0
Tristan Weech <twminebro@gmail.com>
2025-10-10 13:19:51 -0600

    Started to work on UserNavigationHook.ts but I'm unsure how to implement it. I'll have to come back to it later.

--- tweeter-web/src/components/userInfo/UserNavigationHook.ts
+++ tweeter-web/src/components/userInfo/UserNavigationHook.ts
@@ -4,57 +4,88 @@ import { useMessageActions } from "../toaster/MessageHooks";
 import { useUserInfo, useUserInfoActions } from "./UserHooks";
 
 export const useUserNavigation = () => {
-const { displayErrorMessage } = useMessageActions();
+  const { displayErrorMessage } = useMessageActions();
   const { displayedUser, authToken } = useUserInfo();
   const { setDisplayedUser } = useUserInfoActions();
   const navigate = useNavigate();
-  return { navigateToUser: (event: React.MouseEvent) => navigateToUser(event, displayErrorMessage, displayedUser, authToken, setDisplayedUser, navigate) };
-}
 
+  // const listener: UserNavigationView = {
+  //   navigate: navigate,
+  // };
 
+  // const presenterRef = useRef<UserNavigationPresenter | null>(null);
+  // if (!presenterRef.current) {
+  //   presenterRef.current = new UserNavigationPresenter(listener);
+  // }
 
-const navigateToUser = async (event: React.MouseEvent, displayErrorMessage: (message: string, bootstrapClasses?: string | undefined) => string, displayedUser: User | null, authToken: AuthToken | null, setDisplayedUser: (user: User) => void, navigate: NavigateFunction): Promise<void> => {
-    event.preventDefault();
+  return {
+    navigateToUser: (event: React.MouseEvent) =>
+      navigateToUser(
+        event,
+        displayErrorMessage,
+        displayedUser,
+        authToken,
+        setDisplayedUser,
+        navigate
+      ),
+  };
+};
 
-    try {
-        const featureURL = extractFeatureURL(event.target.toString());
-      const alias = extractAlias(event.target.toString());
+const navigateToUser = async (
+  event: React.MouseEvent,
+  displayErrorMessage: (
+    message: string,
+    bootstrapClasses?: string | undefined
+  ) => string,
+  displayedUser: User | null,
+  authToken: AuthToken | null,
+  setDisplayedUser: (user: User) => void,
+  navigate: NavigateFunction
+): Promise<void> => {
+  event.preventDefault();
+  // presenterRef.current!.navigateToUser(
+  //   displayErrorMessage,
+  //   displayedUser,
+  //   authToken,
+  //   setDisplayedUser
+  // );
+  try {
+    const featureURL = extractFeatureURL(event.target.toString());
+    const alias = extractAlias(event.target.toString());
 
-      const toUser = await getUser(authToken!, alias);
+    const toUser = await getUser(authToken!, alias);
 
-      if (toUser) {
-        if (!toUser.equals(displayedUser!)) {
-          setDisplayedUser(toUser);
-          navigate(`${featureURL}/${toUser.alias}`);
-        }
+    if (toUser) {
+      if (!toUser.equals(displayedUser!)) {
+        setDisplayedUser(toUser);
+        navigate(`${featureURL}/${toUser.alias}`);
       }
-    } catch (error) {
-      displayErrorMessage(
-        `Failed to get user because of exception: ${error}`
-      );
     }
-  };
-
-  const extractAlias = (value: string): string => {
-    const index = value.indexOf("@");
-    return value.substring(index);
-  };
-
-  const extractFeatureURL = (value: string): string => {
-    const at_index = value.indexOf("@");
-    const slash_index = value.lastIndexOf("/");
-    if (at_index === -1) {
-      return value.substring(slash_index);
-    }
-    const removedAlias = value.substring(0, at_index - 1);
-    const next_slash = removedAlias.lastIndexOf("/");
-    return removedAlias.substring(next_slash);
+  } catch (error) {
+    displayErrorMessage(`Failed to get user because of exception: ${error}`);
   }
+};
 
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
-  };
+const extractAlias = (value: string): string => {
+  const index = value.indexOf("@");
+  return value.substring(index);
+};
+
+const extractFeatureURL = (value: string): string => {
+  const at_index = value.indexOf("@");
+  const slash_index = value.lastIndexOf("/");
+  if (at_index === -1) {
+    return value.substring(slash_index);
+  }
+  const removedAlias = value.substring(0, at_index - 1);
+  const next_slash = removedAlias.lastIndexOf("/");
+  return removedAlias.substring(next_slash);
+};
+
+const getUser = async (
+  authToken: AuthToken,
+  alias: string
+): Promise<User | null> => {
+  // TODO: Replace with the result of calling server
+  return FakeData.instance.findUserByAlias(alias);
+};
---
+++ tweeter-web/src/presenter/UserNavigationPresenter.ts
@@ -0,0 +1,59 @@
+import { User, AuthToken } from "tweeter-shared";
+import { UserService } from "../model.service/UserService";
+
+export interface UserNavigationView {
+  navigate: (url: string) => void;
+}
+
+export class UserNavigationPresenter {
+  private _view: UserNavigationView;
+  private userService: UserService;
+
+  public constructor(view: UserNavigationView) {
+    this._view = view;
+    this.userService = new UserService();
+  }
+
+  public async navigateToUser(
+    event: React.MouseEvent,
+    displayErrorMessage: (
+      message: string,
+      bootstrapClasses?: string | undefined
+    ) => string,
+    displayedUser: User | null,
+    authToken: AuthToken | null,
+    setDisplayedUser: (user: User) => void
+  ): Promise<void> {
+    try {
+      const featureURL = this.extractFeatureURL(event.target.toString());
+      const alias = this.extractAlias(event.target.toString());
+
+      const toUser = await this.userService.getUser(authToken!, alias);
+
+      if (toUser) {
+        if (!toUser.equals(displayedUser!)) {
+          setDisplayedUser(toUser);
+          this._view.navigate(`${featureURL}/${toUser.alias}`);
+        }
+      }
+    } catch (error) {
+      displayErrorMessage(`Failed to get user because of exception: ${error}`);
+    }
+  }
+
+  extractAlias = (value: string): string => {
+    const index = value.indexOf("@");
+    return value.substring(index);
+  };
+
+  extractFeatureURL = (value: string): string => {
+    const at_index = value.indexOf("@");
+    const slash_index = value.lastIndexOf("/");
+    if (at_index === -1) {
+      return value.substring(slash_index);
+    }
+    const removedAlias = value.substring(0, at_index - 1);
+    const next_slash = removedAlias.lastIndexOf("/");
+    return removedAlias.substring(next_slash);
+  };
+}
