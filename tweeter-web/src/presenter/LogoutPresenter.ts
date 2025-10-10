bd28c86cfcf3ec6e910918c0489bfdac1ffdf279
Tristan Weech <twminebro@gmail.com>
2025-10-09 22:48:56 -0600

    Added LogoutPresenter.ts which is included in AppNavbar.tsx

---
+++ tweeter-web/src/presenter/LogoutPresenter.ts
@@ -0,0 +1,43 @@
+import { AuthToken } from "tweeter-shared";
+import { UserService } from "../model.service/UserService";
+
+export interface LogoutView {
+  displayInfoMessage: (
+    message: string,
+    duration: number,
+    bootstrapClasses?: string | undefined
+  ) => string;
+  deleteMessage: (messageID: string) => void;
+  clearUserInfo: () => void;
+  navigate: (url: string) => void;
+  displayErrorMessage: (message: string) => void;
+}
+
+export class LogoutPresenter {
+  private _view: LogoutView;
+  private userService: UserService;
+
+  public constructor(view: LogoutView) {
+    this._view = view;
+    this.userService = new UserService();
+  }
+
+  public async logOut(authToken: AuthToken | null) {
+    const loggingOutToastId = this._view.displayInfoMessage(
+      "Logging Out...",
+      0
+    );
+
+    try {
+      await this.userService.logout(authToken!);
+
+      this._view.deleteMessage(loggingOutToastId);
+      this._view.clearUserInfo();
+      this._view.navigate("/login");
+    } catch (error) {
+      this._view.displayErrorMessage(
+        `Failed to log user out because of exception: ${error}`
+      );
+    }
+  }
+}
