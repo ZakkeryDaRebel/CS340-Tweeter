71a779d71fbbf8f225809b730c3f3f4d7efe4f0b
Tristan Weech <twminebro@gmail.com>
2025-11-12 13:50:00 -0700

    Finished the Unit Tests for Milestone 3

--- tweeter-web/test/model.service/StatusService.test.ts
+++ tweeter-web/test/model.service/StatusService.test.ts
@@ -1,5 +1,6 @@
-import { AuthToken, Status } from "tweeter-shared";
+import { AuthToken, Status, User } from "tweeter-shared";
 import { StatusService } from "../../src/model.service/StatusService";
+import "isomorphic-fetch";
 
 describe("Test Status Service's loadMoreStoryItems functionality using FakeData", () => {
   const service = new StatusService();
@@ -7,23 +8,33 @@ describe("Test Status Service's loadMoreStoryItems functionality using FakeData"
   const userAlias = "@Allen";
   const pageSize = 5;
   let lastItem: Status | null = null;
+  let fakeDataTenStatuses: Status[] = [];
+  fillFakeDataStatuses();
 
   it("gets 5 story elements with null lastItem", async () => {
     let [items, hasMore] = await getStoryItems();
-    expect(items.length).toBe(5);
+    expect(items.length).toBe(pageSize);
     expect(hasMore).toBe(true);
-    expect(items.at(4)).not.toBeNull();
+    for (let i = 0; i < items.length; i++) {
+      compareItems(items.at(i)!, i);
+    }
     lastItem = items.at(4)!;
-    expect(lastItem.post).not.toBeNull();
-    expect(lastItem.user).not.toBeNull();
   });
   it("gets 5 story elements with a valid lastItem and not have lastItem either", async () => {
     let [items, hasMore] = await getStoryItems();
     expect(items.length).toBe(5);
-    items.forEach((status) => {
-      expect(status).not.toEqual(lastItem);
-    });
+    expect(hasMore).toBe(true);
+    for (let i = 0; i < items.length; i++) {
+      compareItems(items.at(i)!, i + pageSize);
+      expect(items.at(i)).not.toEqual(lastItem);
+    }
   });
+
+  function compareItems(item: Status, num: number) {
+    expect(item).not.toBeNull();
+    expect(item.post).toEqual(fakeDataTenStatuses.at(num)!.post);
+    expect(item.user).toEqual(fakeDataTenStatuses.at(num)!.user);
+  }
 
   async function getStoryItems(): Promise<[Status[], boolean]> {
     return await service.loadMoreStoryItems(
@@ -32,5 +43,118 @@ describe("Test Status Service's loadMoreStoryItems functionality using FakeData"
       pageSize,
       lastItem
     );
+  }
+
+  function fillFakeDataStatuses() {
+    let firstStatus = new Status(
+      "Post 0 0\n        My friend @amy likes this website: http://byu.edu. Do you? \n        Or do you prefer this one: http://cs.byu.edu?",
+      new User(
+        "Allen",
+        "Anderson",
+        "@allen",
+        "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png"
+      ),
+      0
+    );
+    let secondStatus = new Status(
+      "Post 0 1\n        My friend @bob likes this website: http://byu.edu. Do you? \n        Or do you prefer this one: http://cs.byu.edu?",
+      new User(
+        "Amy",
+        "Ames",
+        "@amy",
+        "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/daisy_duck.png"
+      ),
+      0
+    );
+    let thirdStatus = new Status(
+      "Post 0 2\n        My friend @bonnie likes this website: http://byu.edu. Do you? \n        Or do you prefer this one: http://cs.byu.edu?",
+      new User(
+        "Bob",
+        "Bobson",
+        "@bob",
+        "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png"
+      ),
+      0
+    );
+    let fourthStatus = new Status(
+      "Post 0 3\n        My friend @chris likes this website: http://byu.edu. Do you? \n        Or do you prefer this one: http://cs.byu.edu?",
+      new User(
+        "Bonnie",
+        "Beatty",
+        "@bonnie",
+        "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/daisy_duck.png"
+      ),
+      0
+    );
+    let fifthStatus = new Status(
+      "Post 0 4\n        My friend @cindy likes this website: http://byu.edu. Do you? \n        Or do you prefer this one: http://cs.byu.edu?",
+      new User(
+        "Chris",
+        "Colston",
+        "@chris",
+        "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png"
+      ),
+      0
+    );
+    let sixthStatus = new Status(
+      "Post 0 5\n        My friend @dan likes this website: http://byu.edu. Do you? \n        Or do you prefer this one: http://cs.byu.edu?",
+      new User(
+        "Cindy",
+        "Coats",
+        "@cindy",
+        "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/daisy_duck.png"
+      ),
+      0
+    );
+    let seventhStatus = new Status(
+      "Post 0 6\n        My friend @dee likes this website: http://byu.edu. Do you? \n        Or do you prefer this one: http://cs.byu.edu?",
+      new User(
+        "Dan",
+        "Donaldson",
+        "@dan",
+        "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png"
+      ),
+      0
+    );
+    let eighthStatus = new Status(
+      "Post 0 7\n        My friend @elliott likes this website: http://byu.edu. Do you? \n        Or do you prefer this one: http://cs.byu.edu?",
+      new User(
+        "Dee",
+        "Dempsey",
+        "@dee",
+        "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/daisy_duck.png"
+      ),
+      0
+    );
+    let ninthStatus = new Status(
+      "Post 0 8\n        My friend @elizabeth likes this website: http://byu.edu. Do you? \n        Or do you prefer this one: http://cs.byu.edu?",
+      new User(
+        "Elliott",
+        "Enderson",
+        "@elliott",
+        "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png"
+      ),
+      0
+    );
+    let tenthStatus = new Status(
+      "Post 0 9\n        My friend @frank likes this website: http://byu.edu. Do you? \n        Or do you prefer this one: http://cs.byu.edu?",
+      new User(
+        "Elizabeth",
+        "Engle",
+        "@elizabeth",
+        "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/daisy_duck.png"
+      ),
+      0
+    );
+    fakeDataTenStatuses.push(firstStatus);
+    fakeDataTenStatuses.push(secondStatus);
+    fakeDataTenStatuses.push(thirdStatus);
+    fakeDataTenStatuses.push(fourthStatus);
+    fakeDataTenStatuses.push(fifthStatus);
+    fakeDataTenStatuses.push(sixthStatus);
+    fakeDataTenStatuses.push(seventhStatus);
+    fakeDataTenStatuses.push(eighthStatus);
+    fakeDataTenStatuses.push(ninthStatus);
+    fakeDataTenStatuses.push(tenthStatus);
   }
 });
--- tweeter-web/test/network/ServerFacade.test.ts
+++ tweeter-web/test/network/ServerFacade.test.ts
@@ -1,14 +1,44 @@
-import { PagedItemRequest, UserDto } from "tweeter-shared";
+import {
+  AuthToken,
+  PagedItemRequest,
+  PutItemRequest,
+  RegisterRequest,
+  User,
+  UserDto,
+} from "tweeter-shared";
 import { ServerFacade } from "../../src/network/ServerFacade";
 import "isomorphic-fetch";
 
 describe("Test ServerFacade functionality using FakeData", () => {
   const serverFacade = new ServerFacade();
-  const token = "1234";
-  const userAlias = "@Allen";
+  const userAlias = "@allen";
+  const password = "1234";
+  const firstName = "Allen";
+  const lastName = "Anderson";
   const pageSize = 5;
 
-  it("registers a new user", () => {});
+  let fakeDataUsers: User[] = [];
+  instantiateFakeData();
+
+  let token: string;
+
+  it("registers a new user", async () => {
+    const request: RegisterRequest = {
+      alias: userAlias,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+      imageStringBase64: "doesn't matter",
+      imageFileExtension: "doesn't matter",
+    };
+    let [user, authToken] = await serverFacade.register(request);
+    expect(user).not.toBeNull();
+    expect(user.alias).toEqual(userAlias);
+    expect(user.firstName).toEqual(firstName);
+    expect(user.lastName).toEqual(lastName);
+    expect(authToken).not.toBeNull();
+    token = authToken.token;
+  });
 
   it("gets a list of followers", async () => {
     const request: PagedItemRequest<UserDto> = {
@@ -20,13 +50,62 @@ describe("Test ServerFacade functionality using FakeData", () => {
     let [items, hasMore] = await serverFacade.getMoreFollowers(request);
     expect(items.length).toBe(pageSize);
     expect(hasMore).toBe(true);
-    items.forEach((user) => {
-      expect(user).not.toBeNull();
-    });
-    console.log("Server Facade 1st Follower: " + items.at(1)?.alias);
+    for (let i = 0; i < items.length; i++) {
+      expect(items.at(i)).not.toBeNull();
+      expect(items.at(i)).toEqual(fakeDataUsers.at(i));
+    }
   });
 
-  it("gets the count of people following the user", () => {});
+  it("gets the count of people following the user", async () => {
+    const request: PutItemRequest<UserDto> = {
+      token,
+      item: {
+        firstName: firstName,
+        lastName: lastName,
+        alias: userAlias,
+        imageUrl: "",
+      },
+    };
+    let count = await serverFacade.getFolloweeCount(request);
+    expect(count).not.toBeNull();
+    expect(count).toBeGreaterThanOrEqual(0);
+  });
 
-  it("gets the count of people the user is following", () => {});
+  function instantiateFakeData() {
+    let firstUser = new User(
+      "Amy",
+      "Ames",
+      "@amy",
+      "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/daisy_duck.png"
+    );
+    let secondUser = new User(
+      "Bob",
+      "Bobson",
+      "@bob",
+      "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png"
+    );
+    let thirdUser = new User(
+      "Bonnie",
+      "Beatty",
+      "@bonnie",
+      "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/daisy_duck.png"
+    );
+    let fourthUser = new User(
+      "Chris",
+      "Colston",
+      "@chris",
+      "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/donald_duck.png"
+    );
+    let fifthUser = new User(
+      "Cindy",
+      "Coats",
+      "@cindy",
+      "https://faculty.cs.byu.edu/~jwilkerson/cs340/tweeter/images/daisy_duck.png"
+    );
+    fakeDataUsers.push(firstUser);
+    fakeDataUsers.push(secondUser);
+    fakeDataUsers.push(thirdUser);
+    fakeDataUsers.push(fourthUser);
+    fakeDataUsers.push(fifthUser);
+  }
 });
